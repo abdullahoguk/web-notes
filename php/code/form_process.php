dfeab4abb32e45a126d91d26be204b26f0eff4b4
Abdullah Öğük <aoguk1102@gmail.com>
2015-07-21 15:11:44 +0300

    --

--- php/code/form_process.php
+++ php/code/form_process.php
@@ -11,17 +11,38 @@
 	 </pre>
 	 <br/>
 	 	<?php 
+	 	if(isset($_POST["submit"])){
 
-	 		$uname = $_POST["username"];
-	 		$pass = $_POST["password"];
-	 		if(!$uname==""){
-	 			echo "Your Name is \"".$uname."\" and your password is \"".$pass."\"";}
-	 		else{
-	 			echo "You have not entered a name!";}
+			if (isset($_POST["username"])) {
+	 			$uname = $_POST["username"];}
+	 		else {
+	 			$uname = "blanked";}
+		 		
+		 	if (isset($_POST["password"])) {
+	 			$pass = $_POST["password"];}
+	 		else {
+	 			$pass = "blanked";}	
+		 			 		
+		 	
+		 	if ($uname === "blanked") {
+		 		echo "You have not entered a name! ";}
+		 	if($pass === "blanked"){
+		 		echo "You have not entered a password!";}	
+		 	
+		 	if(!($uname === "blanked") && !($pass==="blanked")){
+	 			echo "Your Name is \"".$uname."\" and your password is \"".$pass."\""."<br/>Processed by form_process";}
+	}
+	else{
+		echo "You cannot came here like that";
+	}
+	 	
+	 	 ?>
+	 	 <br/><br/><br/>
+		<?php 
 	 		
 
-	 	 ?>
-	
+	 	 ?>	
+	 	<a href="form.php">GO BACK</a>
 
 </body>
 </html>
